a96bd76484b86f88f9477c1372ad548d1fec7f3a
JaeMin <dnfktmzm@naver.com>
2023-09-17 22:10:34 +0900

    비즈니스 로직 MemberRepository 추가

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    
+    //4가지의 기능
+    // Optional >> Null값을 처리하기 위한 방법 중 하나이다.(요즘 선호)
+
+    Member save(Member member); //회원정보를 저장하는 기능
+    
+    /* 회원의 아이디 or 이름 찾기 기능 */ 
+    Optional<Member> findById(Long id); 
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //저장소에서 멤버의 이름 중 넘어온 값과 같은 값이 있는지 검사
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        // 추후 반복문을 돌리기 편하게 하기 위하여 List로 반환
+        return new ArrayList<>(store.values());
+    }
+}
